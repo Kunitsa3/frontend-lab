09cab297c7ddd1ea6fefa906cb930bed6e087d53
Olya Kunitsa <Kunitsa3@tut.by>
2022-02-25 14:42:54 +0300

    replace data changes to normalizeRandomCocktailData function

--- workflow/react-project/src/store/randomCocktail/index.jsx
+++ workflow/react-project/src/store/randomCocktail/index.jsx
@@ -20,28 +20,7 @@ const randomCocktailSlice = createSlice({
       state.loading = true;
     },
     [fetchRandomCocktail.fulfilled]: (state, action) => {
-      const cocktailData = normalizeRandomCocktailData(action.payload);
-      state.details.cocktailName = cocktailData?.strDrink;
-      state.details.cocktailPicture = cocktailData?.strDrinkThumb;
-      state.details.instructions = cocktailData?.strInstructions;
-
-      state.details.ingredientsDetails = Object.entries(cocktailData || {}).reduce((acc, [key, value]) => {
-        if (key.includes('strIngredient') && value) {
-          const quantityKey = key.replace('strIngredient', 'strMeasure');
-          const measure = cocktailData[quantityKey];
-
-          return [
-            ...acc,
-            {
-              ingredient: value,
-              quantity: measure?.match(/[\s\d/\/]+/)?.join(''),
-              unit: measure?.match(/[a-z ]/gi)?.join(''),
-            },
-          ];
-        }
-        return acc;
-      }, []);
-
+      state.details = normalizeRandomCocktailData(action.payload);
       state.loading = false;
     },
   },
--- workflow/react-project/src/store/randomCocktail/selectors.jsx
+++ workflow/react-project/src/store/randomCocktail/selectors.jsx
@@ -1,7 +1,6 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-const getState = state => state;
+const getRandomCocktail = state => state.randomCocktail || {};
 
-export const selectRandomCocktailLoading = createSelector(getState, state => state.randomCocktail.loading);
-
-export const selectRandomCocktailDetails = createSelector(getState, state => state.randomCocktail.details);
+export const selectRandomCocktailLoading = createSelector(getRandomCocktail, data => data.loading);
+export const selectRandomCocktailDetails = createSelector(getRandomCocktail, data => data.details);
